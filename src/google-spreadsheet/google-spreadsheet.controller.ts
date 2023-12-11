de9a5f2063af6ac7d972063ddb87a705e96b5525
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-12-11 17:15:09 -0300

    docs: Removendo arquivos

--- src/google-spreadsheet/google-spreadsheet.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { GoogleSpreadsheetService } from './google-spreadsheet.service';
-
-@Controller('google-spreadsheet')
-export class GoogleSpreadsheetController {
-  constructor(
-    private readonly googleSpreadsheetService: GoogleSpreadsheetService,
-  ) {}
-}
