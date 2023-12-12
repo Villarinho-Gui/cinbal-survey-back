beebcfc03862671c542b1f0c3ae5b33d9924a462
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-12-12 11:17:28 -0300

    feat: Adicionando imports e providers no survey module

--- src/survey/survey.module.ts
+++ src/survey/survey.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SurveyService } from './survey.service';
 import { SurveyController } from './survey.controller';
+import { GoogleSpreadsheetModule } from '../google-spreadsheet/google-spreadsheet.module';
 
 @Module({
+  imports: [GoogleSpreadsheetModule],
   controllers: [SurveyController],
-  providers: [SurveyService]
+  providers: [SurveyService],
 })
 export class SurveyModule {}
