694da5afcf9712d3b073847afc0ab01872711529
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-12-12 11:16:21 -0300

    feat: Criando o DTO do survey

--- src/survey/dto/create-survey.dto.ts
+++ src/survey/dto/create-survey.dto.ts
@@ -1,1 +1,35 @@
-export class CreateSurveyDto {}
+import {
+  IsUUID,
+  IsEmpty,
+  IsOptional,
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+} from '@nestjs/class-validator';
+
+export class CreateSurveyDto {
+  @IsUUID()
+  @IsOptional()
+  @IsEmpty({
+    message: 'id is auto generated',
+  })
+  id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  average: string;
+
+  @IsNumber()
+  @IsOptional()
+  @IsEmpty({
+    message: 'created_at is auto generated',
+  })
+  created_at: number;
+
+  @IsNumber()
+  @IsOptional()
+  @IsEmpty({
+    message: 'updated_at is auto generated',
+  })
+  updated_at: string;
+}
