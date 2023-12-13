be08580f287092ba1ea6fa6c028c93773846eccb
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-12-13 13:50:53 -0300

    feat: Configurando o survey service

--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -1,26 +1,92 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateSurveyDto } from './dto/create-survey.dto';
 import { UpdateSurveyDto } from './dto/update-survey.dto';
+import { GoogleSpreadsheetService } from '../google-spreadsheet/google-spreadsheet.service';
+import { GoogleSpreadsheet, GoogleSpreadsheetRow } from 'google-spreadsheet';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class SurveyService {
-  create(createSurveyDto: CreateSurveyDto) {
-    return 'This action adds a new survey';
+  private doc: GoogleSpreadsheet;
+
+  constructor(
+    private readonly googleSpreadSheetService: GoogleSpreadsheetService,
+  ) {
+    this.doc = googleSpreadSheetService.getGoogleSpreadSheetInstance();
   }
 
-  findAll() {
-    return `This action returns all survey`;
+  async create(createSurveyDto: Record<string, string> & CreateSurveyDto) {
+    try {
+      const sheet = this.doc.sheetsByIndex[0];
+
+      createSurveyDto.id = uuidv4();
+      createSurveyDto.created_at = new Date().getTime();
+      createSurveyDto.updated_at = new Date().getTime();
+
+      await sheet.addRow(createSurveyDto);
+    } catch (error) {
+      throw new HttpException(
+        'error creating survey',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} survey`;
+  async findAll() {
+    try {
+      const sheet = this.doc.sheetsByIndex[0];
+      const surveyRows = await sheet.getRows();
+      return this.transformRows(surveyRows);
+    } catch (error) {
+      throw new HttpException(
+        'error get survey',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  update(id: number, updateSurveyDto: UpdateSurveyDto) {
-    return `This action updates a #${id} survey`;
+  async update(id: string, updateSurveyDto: UpdateSurveyDto) {
+    try {
+      const sheet = this.doc.sheetsByIndex[0];
+      const surveyRows = await sheet.getRows();
+
+      const currentRow = surveyRows
+        .map((row, index) => ({
+          rowNumber: index,
+          id: row['_rawData'][0],
+        }))
+        .find((row) => row.id === id);
+
+      if (currentRow === null) {
+        return new HttpException('Survey not found', HttpStatus.NOT_FOUND);
+      }
+
+      updateSurveyDto.updated_at = new Date().getTime();
+
+      Object.keys(updateSurveyDto).forEach((key) => {
+        surveyRows[currentRow.rowNumber].set(key, updateSurveyDto[key]);
+      });
+
+      await surveyRows[currentRow.rowNumber].save();
+
+      return updateSurveyDto;
+    } catch (error) {
+      throw new HttpException(
+        'error updating survey',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} survey`;
+  transformRows(rows: GoogleSpreadsheetRow[]): Record<string, any> {
+    const headers = rows[0]._worksheet.headerValues;
+    return rows.map((row) => {
+      const obj = {};
+      headers.forEach((header, index) => {
+        obj[header] = row['_rawData'][index];
+      });
+
+      return obj;
+    });
   }
 }
