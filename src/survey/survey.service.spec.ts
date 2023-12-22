c395a6a27684570d70cb850d72b6ec330deab317
Guilherme Villarinho <Guivillarinho15@gmail.com>
2023-12-22 15:49:04 -0300

    feat: Criando arquivos de teste

---
+++ src/survey/survey.service.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from "@nestjs/testing"
+import { SurveyService } from "./survey.service"
+import { GoogleSpreadsheetService } from "../google-spreadsheet/google-spreadsheet.service"
+import { ConfigModule, ConfigService } from "@nestjs/config"
+
+describe('SurveyService', () => {
+
+    let surveyService: SurveyService
+
+    beforeEach(async () => {
+
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [ConfigModule.forRoot()],
+            providers: [
+                SurveyService,
+                GoogleSpreadsheetService,
+                ConfigService
+            ]
+        }).compile()
+
+        surveyService = module.get<SurveyService>(SurveyService)
+
+    })
+    test('Validating the definition', () => {
+        expect(surveyService).toBeDefined()
+    })
+
+})
